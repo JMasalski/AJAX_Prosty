70cdffa0a2b34a07751dc136f632c37e1af5de23
JMasalski <masalski.kuba123@gmail.com>
2024-10-30 16:58:26 +0100

    Refactor AJAX request and add new functionality for getting and posting data

---
+++ get.php
@@ -0,0 +1,7 @@
+<?php
+
+    $imie = $_GET['imie'];
+    $nazwisko = $_GET['nazwisko'];
+
+    echo "<h1>Otrzymałem $imie $nazwisko (Metoda GET)</h1>";
+?>
--- skrypty.js
+++ skrypty.js
@@ -3,11 +3,15 @@ const dane = {
     nazwisko: 'Abecki',
 }
 
+baseURL = 'http://localhost/GR1_AJAX_prosty/';
+
 window.onload = function(){
-    document.querySelector('#get_plik').addEventListener("click", ajax_get_plik);   
+    document.querySelector('#get_plik').addEventListener("click", ajax_get_plik);
+    document.querySelector('#get_ob').addEventListener("click", ajax_get_ob);
+    document.querySelector('#post_ob').addEventListener("click", ajax_post_ob);          
 }
 
-ajax_get_plik = function(){
+function ajax_get_plik(){
     const random = Math.random() * 10000;
     let url = `plik.txt?rand=${random}`;
     const xhr = new XMLHttpRequest();
@@ -26,4 +30,34 @@ ajax_get_plik = function(){
         console.log('Żądanie nie wykonalne');
     }
     xhr.send();
+}
+
+
+function ajax_get_ob(){
+    let random = Math.random() * 10000;
+    let url = new URL('get.php', baseURL);
+    url.searchParams.set('imie', dane.imie);
+    url.searchParams.set('nazwisko', dane.nazwisko);
+    url.searchParams.set('rand', random);
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+
+    xhr.onload = () => {
+        if(xhr.status != 200){
+            console.log(`Błąd ${xhr.status}: ${xhr.statusText}`);
+        }
+        else{
+            document.querySelector('#wynik').innerHTML = xhr.response;
+        }
+    }
+    xhr.onerror = function(){
+        console.log('Żądanie nie wykonalne');
+    }
+    xhr.send();
+}    
+
+function ajax_post_ob(){
+    let url = "post.php";
+    
 }
